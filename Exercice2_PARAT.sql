ae061e9bc4524c9cf5340caa44c835507931cf5e
developer <dev@svme>
2025-06-10 11:00:59 +0200

    real final commit

--- Exercice2_PARAT.sql
+++
@@ -1,8 +0,0 @@
--- Selectionner tous les utilisateurs qui ont comme nom Dupont
-select nom From Utilisateur Where nom = 'Dupont'; 
-
--- Selectionner les utilisateur ainsi que leur materiel reservé à condition que la date de debut sois superieur à 2025-05-08
-select idUtilisateur, idMateriel From Reservation Where dateDebut > '2025-05-08';
-
--- Selectionner tous les utilisateurs qui ont un nom qui commence M
-SELECT * FROM Utilisateur WHERE nom LIKE 'M%';
